1d855e1e28257daa3a99b01b4ef39e3af0a56582
admin <admin@admin.ad>
2015-06-27 10:05:36 +0300

    Add PDORepository schema.

---
+++ src/Recipe/Repositories/PDORecipesRepository.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: antarus66
+ * Date: 6/25/15
+ * Time: 8:30 PM
+ */
+
+namespace antarus66\BAHomework3\Recipe\Repositories;
+
+
+class PDORecipesRepository extends AbstractRecipesRepository{
+    public function getRecipe($recipe_name)
+    {
+        /**
+         * TODO: Implement this function
+         */
+    }
+
+    public function addRecipe($recipe)
+    {
+        /**
+         * TODO: Implement this function
+         */
+    }
+
+    public function getRecipesNames()
+    {
+        /**
+         * TODO: Implement this function
+         */
+    }
+}
